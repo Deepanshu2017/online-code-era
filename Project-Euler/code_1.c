bde6b6af42511de59346400068e77e3785f56f85
Deepanshu Thakur <deepanshu2017@hotmail.com>
2014-10-24 01:19:33 +0530

    Create code_1.c

---
+++ Project-Euler/code_1.c
@@ -0,0 +1,41 @@
+/*
+ ============================================================================
+ Name        : ProjectEular_1.c
+ Author      : Deepanshu Thakur
+ Version     : 1.0
+ Copyright   : None
+ Description : Project Eular 1
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	int i=0;
+	long int sum=0;
+	for(i=0;i<1000;i++)
+		if(i%3==0 && i%5!=0)
+			sum+=i;
+		else if(i%5==0 && i%3==0)
+			sum+=i;
+		else if(i%5==0 && i%3!=0)
+			sum+=i;
+	printf("%d",sum);
+	return 0;
+}
+
+
+//More short code
+/*
+
+#include<stdio.h>
+int main(void){
+    unsigned long int sum = 0,i=0;
+    for(i = 1; i < 1000; ++i)
+        if(!(i % 3) || !(i % 5))
+            sum += i;
+    printf("%lu",sum);
+    return 0;
+}
+
+*/
