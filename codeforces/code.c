5c2b716d86293b4912811f532ffbeddb4314f4c2
Deepanshu Thakur <deepanshu2017@hotmail.com>
2014-10-23 23:58:03 +0530

    Create code.c

---
+++ codeforces/code.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : codeForces1.c
+ Author      : Deepanshu Thakur
+ Version     : 1.0
+ Copyright   : None
+ Description : CodeForces #274-1
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	
+	int a=0,b=0,c=0,max=0;
+	scanf("%d\n%d\n%d",&a,&b,&c);
+	max=a+b+c;
+	
+	if(max<(a*b*c))
+		max=a*b*c;
+	
+	if(max<(a+(b*c)))
+		max=a+(b*c);
+	
+	if(max<(a*(b+c)))
+		max=a*(b+c);
+	
+	if(max<((a+b)*c))
+		max=((a+b)*c);
+	
+	printf("%d",max);
+	return 0;
+}
